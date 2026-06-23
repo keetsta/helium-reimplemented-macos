d10a345afb3a49cfa61110fb0f2ce26d98dfa37a
keetsta <keetsta@icloud.com>
2026-06-23 08:21:57 +0300

    Use mozilla-actions/sccache-action to expose GHA cache env to build

--- .github/scripts/github_setup_env_toolchain.sh
+++ .github/scripts/github_setup_env_toolchain.sh
@@ -4,10 +4,6 @@
 
 brew install ninja coreutils --overwrite
 
-if ! command -v sccache 2>&1 >/dev/null; then
-  brew install sccache --overwrite
-fi
-
 # Install Python dependencies from PyPI
 pip3 install httplib2==0.22.0 requests pillow --break-system-packages
 npm i -g appdmg@0.6.6
